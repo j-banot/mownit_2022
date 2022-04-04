6be37b75b7210da92fee5951cbf8943b844fffaf
mr-brightsid3 <banot.jan@gmail.com>
2022-04-04 19:37:07 +0200

    add gsl plot solution

--- L2/L2.py
+++ L2/L2.py
@@ -1,14 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import barycentric_interpolate
+from scipy.interpolate import barycentric_interpolate, CubicSpline, Akima1DInterpolator
 
 
 def fun(x: float):
     return x + np.cos(x * x)
-
-
-def fprintf(stream, format_spec, *args):
-    stream.write(format_spec % args)
 
 
 def main():
@@ -18,19 +14,23 @@ def main():
     x_observed = [None] * 10
     y_observed = [None] * 10
 
-    # input = open("wartosci.txt", "w")
-    # output = open("inter.txt", "w")
-
     dx = (b-a) / steps
 
     for i in range(0, steps):
         x_observed[i] = a + i * dx + 0.5 * np.sin(i * dx)
         y_observed[i] = fun(x_observed[i])
-        # fprintf(input, "%g %g\n", x_observed[i], y_observed[i])
 
     x = np.linspace(min(x_observed), max(x_observed), num=100)
-    y = barycentric_interpolate(x_observed, y_observed, x)
-    plt.plot(x, y, label="barycentric interpolation")
+    x_cubic = np.arange(0, 10)
+    y_barycentric = barycentric_interpolate(x_observed, y_observed, x)
+    y_cubic = CubicSpline(x_observed, y_observed)
+    y_akima = Akima1DInterpolator(x_observed, y_observed)
+    print(y_cubic)
+    plt.plot(x_observed, y_observed, 'ro', label="Values")
+    plt.plot(x, y_barycentric, label="Barycentric Interpolation")
+    plt.plot(x_cubic, y_cubic(x_cubic), label="Cubic Interpolation")
+    plt.plot(x, y_akima(x), label="Akima Interpolation")
+    plt.grid()
     plt.legend()
     plt.show()
 
