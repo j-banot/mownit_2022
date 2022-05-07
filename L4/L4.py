f6ff3fbf574331abafbcf6c01249c866a54bfeca
mr-brightsid3 <banot.jan@gmail.com>
2022-05-07 18:05:39 +0200

    add solution to L4

--- L4/L4.py
+++ L4/L4.py
@@ -1,4 +1,5 @@
 import math
+from scipy import integrate
 
 
 def f1(x: float) -> float:
@@ -13,20 +14,38 @@ def calculate_integral(f, a, b, n):
     dx = (b - a) / n
     integral = 0
 
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         i = i * dx + a
         integral += dx * f(i)
 
-    print(f"Result for {n} intervals: {integral} \n")
+    # print(f"Result for {n} intervals: {integral} \n")
     return integral
 
 
-def main():
-    print("x^2:")
-    calculate_integral(f1, 0, 1, 1000)
+def analyze_integral(f, a, b, expected):
+    results = []
+    for i in range(3, 7):
+        limit = pow(10, (-i))
+        inter = 1
+        running = True
+        while running:
+            result = calculate_integral(f, a, b, inter)
+            error = abs(result - expected)
+            running = error >= limit
+            if not running:
+                results.append((result, error, limit, inter))
+                print(f"Result: {result} error: {error}, expected: {expected}, Limit: {limit} Interwals: {inter} \n")
+            inter += 1
+    return result
 
-    print("1/sqrt(x):")
-    calculate_integral(f2, 0, 1, 1000)
+
+def main():
+    # print("x^2:")
+    # calculate_integral(f1, 0, 1, 1000)
+    #
+    # print("1/sqrt(x):")
+    # calculate_integral(f2, 0, 1, 1000)
+    analyze_integral(f2, 0, 1, 1 / 3.0)
 
 
 if __name__ == "__main__":
